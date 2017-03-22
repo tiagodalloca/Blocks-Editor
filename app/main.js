9a3477ffca0f2a6b86a11cf56b31d51fcc1a68ee
Tiago Dall'Oca <tdalloca@gmail.com>
2017-03-22 11:55:27 -0300

    Hello World from re-frame repository

--- app/main.js
+++ app/main.js
@@ -8,12 +8,12 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+	win = new BrowserWindow({fullscreen: true});
 
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
-    protocol: 'https:',
+    protocol: 'file:',
     slashes: true
   }))
 
--- src/blocks_editor_demo/core.cljs
+++ src/blocks_editor_demo/core.cljs
@@ -2,16 +2,87 @@
   (:require [reagent.core :as reagent]
             [re-frame.core :as rf]))
 
-(defn ui []
-  [:div#app-wrapper
-   [:h1 "Hello world!"]
-   [:p "Basic hw"]
-   [:p "A prova não eh pra semana que vem"]
-   [:marquee
-    [:h2 "Obama"]
-    [:a {:href "http://github.com"} "GitHub"]]])
+;; A detailed walk-through of this source code is provied in the docs:
+;; https://github.com/Day8/re-frame/blob/master/docs/CodeWalkthrough.md
 
-(defn init! [setting]
-  (rf/dispatch-sync [:initialize])
-  (reagent/render [ui] (js/document.getElementById "app")))
+;; -- Domino 1 - Event Dispatch -----------------------------------------------
 
+(defn dispatch-timer-event
+  []
+  (let [now (js/Date.)]
+    (rf/dispatch [:timer now])))  ;; <-- dispatch used
+
+;; Call the dispatching function every second.
+;; `defonce` is like `def` but it ensures only instance is ever
+;; created in the face of figwheel hot-reloading of this file.
+(defonce do-timer (js/setInterval dispatch-timer-event 1000))
+
+
+;; -- Domino 2 - Event Handlers -----------------------------------------------
+
+(rf/reg-event-db              ;; sets up initial application state
+ :initialize                 ;; usage:  (dispatch [:initialize])
+ (fn [_ _]                   ;; the two parameters are not important here, so use _
+   {:time (js/Date.)         ;; What it returns becomes the new application state
+    :time-color "#f88"}))    ;; so the application state will initially be a map with two keys
+
+
+(rf/reg-event-db                ;; usage:  (dispatch [:time-color-change 34562])
+ :time-color-change            ;; dispatched when the user enters a new colour into the UI text field
+ (fn [db [_ new-color-value]]  ;; -db event handlers given 2 parameters:  current application state and event (a vector)
+   (assoc db :time-color new-color-value)))   ;; compute and return the new application state
+
+
+(rf/reg-event-db                 ;; usage:  (dispatch [:timer a-js-Date])
+ :timer                         ;; every second an event of this kind will be dispatched
+ (fn [db [_ new-time]]          ;; note how the 2nd parameter is desctructure to obtain the data value
+   (assoc db :time new-time)))  ;; compute and return the new application state
+
+
+;; -- Domino 4 - Query  -------------------------------------------------------
+
+(rf/reg-sub
+ :time
+ (fn [db _]     ;; db is current app state. 2nd usused param is query vector
+   (-> db
+       :time)))
+
+(rf/reg-sub
+ :time-color
+ (fn [db _]
+   (:time-color db)))
+
+
+;; -- Domino 5 - View Functions ----------------------------------------------
+
+(defn clock
+  []
+  [:div.example-clock
+   {:style {:color @(rf/subscribe [:time-color])}}
+   (-> @(rf/subscribe [:time])
+       .toTimeString
+       (clojure.string/split " ")
+       first)])
+
+(defn color-input
+  []
+  [:div.color-input
+   "Time color: "
+   [:input {:type "text"
+            :value @(rf/subscribe [:time-color])
+            :on-change #(rf/dispatch [:time-color-change (-> % .-target .-value)])}]])  ;; <---
+
+(defn ui
+  []
+  [:div
+   [:h1 "Hello world, it is now"]
+   [clock]
+   [color-input]])
+
+;; -- Entry Point -------------------------------------------------------------
+
+(defn ^:export init!
+  [settings]
+  (rf/dispatch-sync [:initialize])     ;; puts a value into application state
+  (reagent/render [ui]              ;; mount the application's ui into '<div id="app" />'
+                  (js/document.getElementById "app")))
