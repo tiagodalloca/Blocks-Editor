ea4d07f35915fa8391f3a8839ef97dfc83b0a309
MrDallOca <tdalloca@gmail.com>
2017-08-02 11:35:40 -0300

    Added EVENTS + update

--- src/blocks_editor/init.cljs
+++ src/blocks_editor/init.cljs
@@ -9,6 +9,35 @@
   (:require-macros [cljs.core.async.macros
                     :refer [go go-loop alt!]]))
 
+
+(def update-block
+  {:name "UPDATE"
+   :type "update"
+   :lastDummyAlign0 "RIGHT"
+   :message0 "%1 %2"
+   :args0 ["UPDATE"
+           {:type "field_variable", :name "state", :variable "state"}]
+   :nextStatement nil
+   :colour 30
+   :tooltip "Receive the game state and updates it with an action"
+   :helpUrl ""})
+
+
+(defn init-blocks! []
+  (defonce goog (js* "goog"))
+  (.require goog "Blockly.Blocks")
+  (defonce bblocks (js* "Blockly.Blocks"))
+  (defonce bsvg (js* "Blockly.BlockSvg"))
+  
+  (set! (.-update bblocks)
+        (clj->js
+         {:init (fn []
+                  (this-as this
+                    (.jsonInit this
+                               (clj->js update-block))))}))
+  (set! (.-START_HAT bsvg) true))
+
 (go (<! (subscribe-on-wload))
+    (init-blocks!)
     (c/init!))
 
