5b8d87c285461773001486e485a8f68941f6baaf
MrDallOca <tdalloca@gmail.com>
2017-08-02 13:37:49 -0300

    Action added

--- src/blocks_editor/init.cljs
+++ src/blocks_editor/init.cljs
@@ -11,7 +11,7 @@
 
 
 (def update-block
-  {:type "events_update"
+  {:type "game_update"
    :lastDummyAlign0 "RIGHT"
    :message0 "%1 %2"
    :args0 ["UPDATE"
@@ -22,7 +22,7 @@
    :helpUrl ""})
 
 (def return-block
-  {:type "events_return"
+  {:type "game_return"
    :lastDummyAlign0 "RIGHT"
    :message0 "%1 %2"
    :args0 ["return action"
@@ -30,6 +30,17 @@
    :previousStatement nil
    :colour 30
    :tooltip "Return an action to be handled by the game"
+   :helpUrl ""})
+
+(def action-block
+  {:type "game_action"
+   :lastDummyAlign0 "RIGHT"
+   :message0 "%1 %2"
+   :args0 ["action"
+           {:type "field_input", :name "ACTION_NAME", :text "walk", :spellcheck false}]
+   :output "ActionBlock"
+   :colour 250
+   :tooltip "Something to do and its args"
    :helpUrl ""})
 
 ;; (defn def-blocks [bindings]
@@ -55,19 +66,26 @@
   (defonce bblocks (js* "Blockly.Blocks"))
   (defonce bsvg (js* "Blockly.BlockSvg"))
   
-  (set! (.-events_update bblocks)
+  (set! (.-game_update bblocks)
         (clj->js
          {:init (fn []
                   (this-as this
                     (.jsonInit this
                                (clj->js update-block))))}))
 
-  (set! (.-events_return bblocks)
+  (set! (.-game_return bblocks)
         (clj->js
          {:init (fn []
                   (this-as this
                     (.jsonInit this
                                (clj->js return-block))))}))
+
+  (set! (.-game_action bblocks)
+        (clj->js
+         {:init (fn []
+                  (this-as this
+                    (.jsonInit this
+                               (clj->js action-block))))}))
   
   (set! (.-START_HAT bsvg) true))
 
